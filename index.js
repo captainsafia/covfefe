644228dde29ae1b7898144a1ac32aa37ca72553d
Bryan English <bryan@bryanenglish.com>
2017-05-30 23:08:38 -0700

    make it a bin

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
 module.exports = function(input) {
   return input + ' covfefe';
+}
+
+if (require.main === module) {
+  console.log(module.exports(process.argv.slice(2).join(' ')));
 }
